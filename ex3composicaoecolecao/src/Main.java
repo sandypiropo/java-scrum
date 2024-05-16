7129271a8124a751b0e5967ebc581d8496784083
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-05-16 19:56:00 -0300

    ex3 att

--- ex3composicaoecolecao/src/Main.java
+++ ex3composicaoecolecao/src/Main.java
@@ -1,3 +1,15 @@
+//Uma empresa deseja automatizar a sua folha de pagamento.
+// A empresa tem o nome e o cnpj e uma coleção de empregados.
+// Cada empregado tem nome, cpf, função e salário mensal.
+// Escreva em java funcionalidades para :
+// 1) Contratar um empregado,
+// 2) Demitir um funcionário indicando o seu cpf,
+// 3) Emitir o relatório da folha de pagamento mensal da seguinte.
+// A folha é iniciada passando o mês e opcionalmente um percentual de bônus
+// para o mês, exemplo (dezembro, 100). Neste caso o salário do funcionário receberá
+// um acréscimo de 100%.  O relatório deve ter no cabeçalho os dados da empresa,
+// mês e bônus. Segue então a lista dos funcionários com cpf, nome e salário total.
+
 public class Main {
     public static void main(String[] args) {
 
